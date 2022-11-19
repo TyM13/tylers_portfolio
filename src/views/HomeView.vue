a3212e7d33cca390f062d6cf28bdcb9ef305ba91
Your Name <you@example.com>
2022-11-18 22:29:15 -0700

    set up pages and configured debugger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main-page />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainPage from "../components/mainPage.vue";
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    MainPage,
+  },
+};
 </script>
